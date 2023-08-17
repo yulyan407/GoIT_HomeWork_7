f5dddb30b65220d167f879df44255aa2bceda2b1
yulyan407 <yu_lyan@ukr.net>
2023-08-17 17:02:57 +0300

    Third_commit

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -1,19 +1,14 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup
 
 setup(
     name='clean_folder',
-    version='0.0.1',
+    version='1',
     description='Clean folder program by Python',
     author='yulyan407',
     author_email='yu_lyan@ukr.net',
     url='https://github.com/yulyan407/GoIT_HomeWork_7',
     license='MIT',
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
-    ],
-    packages=find_namespace_packages(),
+    packages=['clean_folder'],
     include_package_data=True,
     entry_points={'console_scripts': ['clean-folder = clean_folder.clean:main']}
 )
